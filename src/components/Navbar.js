6716e70712ee7e59763500a598aedafc63b3c424
jason-701 <101868817+jason-701@users.noreply.github.com>
2023-06-16 11:39:58 +0800

    removed unnecessary comments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,3 @@
-/*import React from "react";
-import "./styles.css";
-
-const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <ul>
-        <li>
-          <a href="#home">Home</a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#portfolio">Portfolio</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
-    </nav>
-  );
-};
-
-export default Navbar;*/
-
 import React, { useEffect, useState } from "react";
 import "./styles.css";
 

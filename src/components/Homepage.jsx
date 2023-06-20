46efb118d8b2567785f2ea1b6c9b3a9e5bff6fd9
jason-701 <101868817+jason-701@users.noreply.github.com>
2023-06-20 12:26:06 +0800

    testing out flex boxes

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,10 +4,20 @@ import "./styles.css";
 const Homepage = () => {
   return (
     <section id="home" className="homepage">
-      <h1>
+      <p className="notes">
         Hi! I am Chang Chieh Hsiang, a computer engineering student from Nanyang
         Technological University
-      </h1>
+      </p>
+      <p>p.s Image credits to google (I will change it later)</p>
+      <img src={require("../images/doge.jpg")} alt="doge" />
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
       <br></br>
       <br></br>
       <br></br>

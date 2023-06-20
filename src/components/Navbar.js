d736bca97ff79dc49d194de1ca6721ff1be127d0
jason-701 <101868817+jason-701@users.noreply.github.com>
2023-06-20 11:56:31 +0800

    fixed some navbar animation issues

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,26 +3,17 @@ import "./styles.css";
 
 const Navbar = () => {
   const [activeSection, setActiveSection] = useState("home");
+  const sectionIds = ["home", "about", "project", "contact"];
 
   useEffect(() => {
     const handleScroll = () => {
-      const homeSection = document.getElementById("home");
-      const aboutSection = document.getElementById("about");
-      const portfolioSection = document.getElementById("project");
-      const contactSection = document.getElementById("contact");
-      const sections = [
-        homeSection,
-        aboutSection,
-        portfolioSection,
-        contactSection,
-      ];
-
       const currentPosition = window.pageYOffset + window.innerHeight / 2;
       let currentSection = "home";
 
-      sections.forEach((section) => {
+      sectionIds.forEach((sectionId) => {
+        const section = document.getElementById(sectionId);
         if (section && currentPosition >= section.offsetTop) {
-          currentSection = section.id;
+          currentSection = sectionId;
         }
       });
 
@@ -33,8 +24,8 @@ const Navbar = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  const handleClick = (section) => {
-    setActiveSection(section);
+  const handleClick = (section, event) => {
+    event.preventDefault();
 
     const element = document.getElementById(section);
     if (element) {
@@ -42,24 +33,31 @@ const Navbar = () => {
         top: element.offsetTop,
         behavior: "smooth",
       });
+
+      // Update the active section after the scrolling animation finishes
+      const scrollTimeout = setTimeout(() => {
+        setActiveSection(section);
+        clearTimeout(scrollTimeout);
+      }, 1000); // Adjust the timeout value as needed
     }
   };
 
   return (
     <nav className="navbar">
       <ul>
-        <li className={activeSection === "home" ? "active" : ""}>
-          <button onClick={() => handleClick("home")}>Home</button>
-        </li>
-        <li className={activeSection === "about" ? "active" : ""}>
-          <button onClick={() => handleClick("about")}>About</button>
-        </li>
-        <li className={activeSection === "project" ? "active" : ""}>
-          <button onClick={() => handleClick("project")}>Projects</button>
-        </li>
-        <li className={activeSection === "contact" ? "active" : ""}>
-          <button onClick={() => handleClick("contact")}>Contact</button>
-        </li>
+        {sectionIds.map((sectionId) => (
+          <li
+            key={sectionId}
+            className={activeSection === sectionId ? "active" : ""}
+          >
+            <a
+              href={`#${sectionId}`}
+              onClick={(e) => handleClick(sectionId, e)}
+            >
+              {sectionId.charAt(0).toUpperCase() + sectionId.slice(1)}
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
   );

4d2ce50a146fd2ab3345b493566e95a3d00891c7
jason-701 <101868817+jason-701@users.noreply.github.com>
2023-06-20 12:31:07 +0800

    center align text

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,11 +4,10 @@ import "./styles.css";
 const Homepage = () => {
   return (
     <section id="home" className="homepage">
-      <p className="notes">
-        Hi! I am Chang Chieh Hsiang, a computer engineering student from Nanyang
-        Technological University
+      <p className="notes">Chang Chieh Hsiang, Y3 Computer Engineering, NTU</p>
+      <p className="temp">
+        p.s Image credits to google (I will change it later)
       </p>
-      <p>p.s Image credits to google (I will change it later)</p>
       <img src={require("../images/doge.jpg")} alt="doge" />
       <br></br>
       <br></br>

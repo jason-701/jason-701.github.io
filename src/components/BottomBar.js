50cedee8b4e80cca6a95caed623d8e5879bb6c92
jason-701 <101868817+jason-701@users.noreply.github.com>
2023-06-26 16:34:58 +0800

    small change

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -4,14 +4,18 @@ import "./styles.css";
 const BottomBar = () => {
   return (
     <div className="bottomBar">
-      <a href="https://github.com/jason-701/" target="_blank">
+      <a href="https://github.com/jason-701/" target="_blank" rel="noreferrer">
         <img
           src={require("../images/github.png")}
           alt="github"
           className="logo"
         />
       </a>
-      <a href="https://www.instagram.com/jason.chang1/" target="_blank">
+      <a
+        href="https://www.instagram.com/jason.chang1/"
+        target="_blank"
+        rel="noreferrer"
+      >
         <img
           src={require("../images/instagram.png")}
           alt="Logo 3"
@@ -21,6 +25,7 @@ const BottomBar = () => {
       <a
         href="https://sg.linkedin.com/in/chieh-hsiang-chang-b080ab250"
         target="_blank"
+        rel="noreferrer"
       >
         <img
           src={require("../images/linkedin.png")}
